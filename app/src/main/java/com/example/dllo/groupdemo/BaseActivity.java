e9da86c8bdc7159841d61b39179fb8ee0dff5d5d
卢小玉 <923121067@qq.com>
2016-11-18 20:19:58 +0800

    BaseActivity

---
+++ app/src/main/java/com/example/dllo/groupdemo/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.example.dllo.groupdemo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/11/18.
+ */
+public class BaseActivity extends AppCompatActivity{
+    // 组长写的
+}
